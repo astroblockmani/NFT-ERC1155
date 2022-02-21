32e0238147b05d6a9d350d0f2a1bc6d68dfb85ee
FManiR <18592101+fmanir@users.noreply.github.com>
2022-02-21 03:58:14 +0000

    Initial Commit

---
+++ nft101/scripts/deploy.js
@@ -0,0 +1,28 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const SuperMarioWorld = await ethers.getContractFactory(
+    "SuperMarioWorldERC1155"
+  );
+  const superMarioWorld = await SuperMarioWorld.deploy(
+    "SuperMarioWorldERC1155",
+    "SPRME"
+  );
+
+  await superMarioWorld.deployed();
+  console.log("Success! Contract was deployed to ", superMarioWorld.address);
+
+  await superMarioWorld.mint(
+    10,
+    "https://ipfs.io/ipfs/QmNRTxq74nttvc2kHXaVbEb1AeSLbDEiCWhiAFz94kooem"
+  );
+
+  console.log("NFT successfully minted");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
